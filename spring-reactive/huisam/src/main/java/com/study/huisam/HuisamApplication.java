ade301a687d5f9acc291d59d7ce9699c9017ddd4
huisam <huisam@naver.com>
2020-12-05 16:50:57 +0900

    feat(reactive): multi module project 준비

---
+++ spring-reactive/huisam/src/main/java/com/study/huisam/HuisamApplication.java
@@ -0,0 +1,11 @@
+package com.study.huisam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuisamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HuisamApplication.class);
+    }
+}
---
+++ spring-reactive/minhyungPark/src/main/java/com/study/minhyungpark/MinhyungParkApplication.java
@@ -0,0 +1,11 @@
+package com.study.minhyungpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhyungParkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MinhyungParkApplication.class);
+    }
+}
